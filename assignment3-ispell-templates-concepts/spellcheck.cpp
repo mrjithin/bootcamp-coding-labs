89c6a5cd35c387ec060b1fa80095d4bea3b48910
mrjithin <silverflames007@gmail.com>
2026-05-26 23:46:41 +0530

    Completed tokenize() from assignment 3

--- assignment3-ispell-templates-concepts/spellcheck.cpp
+++ assignment3-ispell-templates-concepts/spellcheck.cpp
@@ -12,7 +12,18 @@ std::vector<Iterator> find_all(Iterator begin, Iterator end, UnaryPred pred);
 
 Corpus tokenize(std::string& source) {
   /* TODO: Implement this method */
-  return Corpus();
+  auto space_its=find_all(source.begin(),source.end(),::isspace);
+  
+  Corpus tokens;
+  std::transform(space_its.begin(),space_its.end()-1,space_its.begin()+1,std::inserter(tokens,tokens.end()),[&source](const auto& it1, const auto& it2){
+    return Token(source,it1,it2);
+  });
+
+  std::erase_if(tokens,[](const auto& token){
+    return token.content.empty();
+  });
+
+  return tokens;
 }
 
 std::set<Misspelling> spellcheck(const Corpus& source, const Dictionary& dictionary) {
